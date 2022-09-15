3e9af6fb8fb409df3e0fb08915361893d6409621
nlif <m.payunen@gmail.com>
2022-09-15 17:40:24 +0300

    Start using stdlib 'url.PathEscape' func instead of self write func

--- atomgen.go
+++ atomgen.go
@@ -5,101 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	// "os"
+	"net/url"
 	"path/filepath"
 	"strconv"
-	//	"time"
 )
-
-// https://www.tutorialspoint.com/html/html_url_encoding.htm
-func GetRidOfWrongCharacters(filename string) (rightName string) {
-OuterLoop:
-	for _, char := range filename {
-		switch char {
-		// Reserved characters
-		case '$':
-			rightName += "%24"
-			continue OuterLoop
-		case '&':
-			rightName += "%26"
-			continue OuterLoop
-		case '+':
-			rightName += "%2b"
-			continue OuterLoop
-		case ',':
-			rightName += "%2c"
-			continue OuterLoop
-		case '/':
-			rightName += "%2f"
-			continue OuterLoop
-		case ':':
-			rightName += "%3a"
-			continue OuterLoop
-		case ';':
-			rightName += "%3b"
-			continue OuterLoop
-		case '=':
-			rightName += "%3d"
-			continue OuterLoop
-		case '?':
-			rightName += "%3f"
-			continue OuterLoop
-		case '@':
-			rightName += "%40"
-			continue OuterLoop
-		// Unsafe characters
-		case ' ':
-			rightName += "%20"
-			continue OuterLoop
-		case '"':
-			rightName += "%22"
-			continue OuterLoop
-		case '<':
-			rightName += "%3c"
-			continue OuterLoop
-		case '>':
-			rightName += "%3e"
-			continue OuterLoop
-		case '#':
-			rightName += "%23"
-			continue OuterLoop
-		case '%':
-			rightName += "%25"
-			continue OuterLoop
-		case '{':
-			rightName += "%7b"
-			continue OuterLoop
-		case '}':
-			rightName += "%7d"
-			continue OuterLoop
-		case '|':
-			rightName += "%7c"
-			continue OuterLoop
-		case '\\':
-			rightName += "%5c"
-			continue OuterLoop
-		case '^':
-			rightName += "%5e"
-			continue OuterLoop
-		case '~':
-			rightName += "%7e"
-			continue OuterLoop
-		case '[':
-			rightName += "%5b"
-			continue OuterLoop
-		case ']':
-			rightName += "%5d"
-			continue OuterLoop
-		case '`':
-			rightName += "%60"
-			continue OuterLoop
-		default:
-			rightName += string(char)
-
-		}
-	}
-	return rightName
-}
 
 func getLinkType(filename string) (linkType string) {
 	ext := filepath.Ext(filename)
@@ -166,7 +75,7 @@ func main() {
 			continue
 		}
 
-		urlEncodedName := GetRidOfWrongCharacters(Name)
+		urlEncodedName := url.PathEscape(Name)
 		fileLoc := channelLink + "/" + srcFolder + "/" + urlEncodedName
 		fmt.Printf("Generated entry for '%s' %s\n", Name, file.ModTime().String())
 		entries = append(entries, Entry{Title: Name,
--- atomgen_test.go
+++ atomgen_test.go
@@ -1,21 +1,6 @@
 package main
 
-import "testing"
-
-func TestGetRidOfWrongCharacters(t *testing.T) {
-	tables := []struct {
-		testCase string
-		answer   string
-	}{
-		{"Hello world  peace", "Hello%20world%20%20peace"},
-		{"HelloWorld", "HelloWorld"},
-		{"Hello World", "Hello%20World"},
-		{"Hello&World", "Hello%26World"},
-	}
-	for _, table := range tables {
-		rightName := GetRidOfWrongCharacters(table.testCase)
-		if rightName != table.answer {
-			t.Errorf("RightName was incorrect, got: %s, want: %s.", rightName, table.answer)
-		}
-	}
-}
+import (
+	"net/url"
+	"testing"
+)
