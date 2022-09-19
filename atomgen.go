8afd05ff0145f45dcfb9735900f1cd1acd8f9ec5
nlif <m.payunen@gmail.com>
2022-09-19 21:22:26 +0300

    Start working on download files from golang with yt-dlp.

--- atomgen.go
+++ atomgen.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/xml"
 	"fmt"
 	"io"
@@ -9,6 +10,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
 	"time"
@@ -16,43 +18,73 @@ import (
 
 const (
 	DetectContentTypeMost = 512
+
+	YTDLP                  = "yt-dlp"
+	SRC_FOLDER             = "src"
+	URLS_CSV_FILE          = "urls.csv"
+	YTDLP_DOWNLOAD_ARCHIVE = SRC_FOLDER + string(os.PathSeparator) + "downloaded.txt"
+	YTDLP_OUTPUT_TEMPLATE  = SRC_FOLDER + string(os.PathSeparator) + "%(uploader)s_%(title)s.%(ext)s"
+
+	ATOM_FILE = "rss.xml"
+
+	CHANNEL_TITLE = "test page"
+	CHANNEL_LINK  = "https://rss.yasal.xyz"
 )
 
-func main() {
-	channelTitle := "test page"
-	channelLink := "https://rss.yasal.xyz"
+// TODO: pass YTDLP arguments as some type of struct for example
+// to avoid using global variables
 
-	ytDlAlreadyDownloadFile := "downloaded.txt"
+func downloadChannelAsAudio(channelUrl string) (err error) {
+	cmd := exec.Command(YTDLP, "--playlist-end", "10", "--dateafter", "today-4weeks", "-x", "--download-archive", YTDLP_DOWNLOAD_ARCHIVE, "-f",
+		"bestaudio", "-o", YTDLP_OUTPUT_TEMPLATE, channelUrl)
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	return err
+}
 
-	rssFile := "rss.xml"
-	srcFolder := "src"
+func downloadVideoAsAudio(_url string) (err error) {
+	cmd := exec.Command(YTDLP, "-x", "--download-archive", YTDLP_DOWNLOAD_ARCHIVE, "-f",
+		"bestaudio", "-o", YTDLP_OUTPUT_TEMPLATE, _url)
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	return err
+}
 
-	files, err := ioutil.ReadDir(srcFolder)
+func downloadVideosFromFile(file string) {
+	fd, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(fd)
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	length := len(records)
+	for index, record := range records {
+		source := record[0]
+		err = downloadChannelAsAudio(source)
+		log.Printf("[%d/%d]\t%s", index+1, length, source)
+	}
+
+}
+
+func generateAtomRssFile(rssFile string) {
+
+	files, err := ioutil.ReadDir(SRC_FOLDER)
 	if err != nil {
 		panic(err)
 	}
 
 	var entries = make([]Entry, 0, 10)
-	// for _, file := range files {
-	// 	oldName := srcFolder + "/" + file.Name()
-
-	// 	oldExt := filepath.Ext(oldName)
-	// 	if oldExt == ".part" {
-	// 		continue
-	// 	}
-	// 	rightName := GetRidOfWrongCharacters(file.Name())
-	// 	newName := srcFolder + "/" + rightName
-	// 	if oldName != newName {
-	// 		err = os.Rename(oldName, newName)
-	// 		if err != nil {
-	// 			fmt.Fprintf(os.Stderr, "ERROR: when renaming %s to %s", oldName, newName)
-	// 			panic(err)
-	// 		}
-	// 		fmt.Printf("Rename '%s' to '%s'\n", oldName, newName)
-	// 	}
-	// }
-
-	files, err = ioutil.ReadDir(srcFolder)
+	files, err = ioutil.ReadDir(SRC_FOLDER)
 	if err != nil {
 		panic(err)
 	}
@@ -64,7 +96,7 @@ func main() {
 			continue
 		}
 
-		if Name == ytDlAlreadyDownloadFile {
+		if Name == filepath.Base(YTDLP_DOWNLOAD_ARCHIVE) {
 			fmt.Printf("%s \n", Name)
 			continue
 		}
@@ -87,9 +119,9 @@ func main() {
 		mimeType := http.DetectContentType(head)
 
 		urlEncodedName := url.PathEscape(Name)
-		fileLoc := channelLink + "/" + srcFolder + "/" + urlEncodedName
+		fileLoc := CHANNEL_LINK + "/" + SRC_FOLDER + "/" + urlEncodedName
 		fileTime := file.ModTime().Format(time.RFC3339)
-		fmt.Printf("Generated entry for '%s' %s\n", Name, fileTime)
+		log.Printf("Generated entry for '%s': '%s'\t%s\n", ATOM_FILE, Name, fileTime)
 		entries = append(entries,
 			Entry{
 				Title: Name,
@@ -104,11 +136,11 @@ func main() {
 
 	v := &Atom{
 		Xmlns:   "http://www.w3.org/2005/Atom",
-		Title:   channelTitle,
-		Link:    Link{Href: channelLink},
+		Title:   CHANNEL_TITLE,
+		Link:    Link{Href: CHANNEL_LINK},
 		Updated: time.Now().Format(time.RFC3339),
 		Author:  Author{Name: "rss.yasal.xyz"},
-		Id:      channelLink,
+		Id:      CHANNEL_LINK,
 		Entries: entries}
 
 	data, err := xml.MarshalIndent(v, " ", "  ")
@@ -123,4 +155,14 @@ func main() {
 		panic(err)
 
 	}
+}
+
+func main() {
+	log.Println("Start downloading videos from urls in ", URLS_CSV_FILE)
+	downloadVideosFromFile(URLS_CSV_FILE)
+	log.Println("Finished downloading videos from urls in ", URLS_CSV_FILE)
+
+	log.Println("Start generating ", ATOM_FILE)
+	generateAtomRssFile(ATOM_FILE)
+	log.Println("Finish generating ", ATOM_FILE)
 }
