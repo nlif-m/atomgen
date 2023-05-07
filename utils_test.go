0710ea6c97c2925cd765abe3dbdde0ecaaeab5d4
nlif <m.payunen@gmail.com>
2023-05-07 23:34:53 +0300

    feat: implement "In" function to check is element in slice

--- utils_test.go
+++ utils_test.go
@@ -29,3 +29,28 @@ func TestUnique(t *testing.T) {
 	}
 
 }
+
+func TestIn(t *testing.T) {
+	type testCase[T comparable] struct {
+		item  T
+		items []T
+		flag  bool
+	}
+
+	tests := []testCase[string]{
+		{"mp3", YtdlpAudioFormats[:], true},
+		{"mp2", YtdlpAudioFormats[:], false},
+		{"", []string{"Hello", "World"}, false},
+	}
+
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s in %s is %v", test.item, test.items, test.flag)
+		t.Run(testname, func(t *testing.T) {
+			answer := In(test.item, test.items)
+			if answer != test.flag {
+				t.Errorf("got %v, want %v", answer, test.flag)
+			}
+
+		})
+	}
+}
